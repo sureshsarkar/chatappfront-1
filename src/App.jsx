c69750293916cf8a8eb217e9075bc968333d9b38
sureshsarkar123 <suresh.sarkar@digitalhubsolution.com>
2024-08-27 18:56:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import Login from './pages/login/Login.jsx';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import './App.css'
 import SignUp from './pages/signup/SignUp.jsx';
 import Home from './pages/home/Home.jsx';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +13,7 @@ function App() {
         <Router>
           <Routes>
             <Route exact path="/" element={<Home/>}/>
-            <Route exact path="/" element={<Login/>}/>
+            <Route exact path="/login" element={<Login/>}/>
             <Route exact path="/signup" element={<SignUp/>}/>
           </Routes>
         </Router>
